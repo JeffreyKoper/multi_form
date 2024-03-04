32483b61fc3ba87a84e507ba14a174e7bb5b0482
JeffreyKoper <jkoper257@student.novacollege.nl>
2024-03-04 11:56:10 +0100

    javascript now takes the email that is given and puts it in the thanks page's description.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const form = document.getElementById('form');
 const thanks = document.getElementById('thanks');
 const dismissButton = document.getElementById('dismissButton');
+const EmailInput = document.getElementById('email_input');
+const email = document.getElementById('email');
 
 
 // Add event listener to the form submit button
@@ -8,6 +10,8 @@ form.addEventListener('submit', function(event) {
   event.preventDefault(); 
   thanks.style.display = 'flex'; 
   form.style.display = 'none';
+  const emailValue = EmailInput.value;
+  email.innerHTML = emailValue;
 });
 
 // Add event listener to the dismiss button
